331b4bea27f210f32e70cb4f9cc6b673e3743722
ivenyarovoy <iven.yarovoy@gmail.com>
2023-05-20 23:48:51 -0700

    Added card and deck internal functions

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 class Card:
-    def __init__(self):
-        pass
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+    def show(self):
+        print("{} of {}".format(self.value, self.suit))
 
 class Deck:
     def __init__(self):
-        pass
+        self.cards = []
+        self.build()
+
+    def build(self):
+        for suit in ["Hearts", "Diamonds", "Clubs", "Spades"]:
+            for value in range(1, 14):
+                self.cards.append(Card(suit, value))
+
+    def show(self):
+        for card in self.cards:
+            card.show()
+
 class Player:
     def __init__(self):
         pass
 
+
+deck = Deck()
+deck.show()
