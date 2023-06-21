b6b5c168e335247d3922375374cf1fb212b70fe8
ivenyarovoy <iven.yarovoy@gmail.com>
2023-06-21 14:49:37 -0700

    Added an init clause and a faulty show command

--- main.py
+++ main.py
@@ -49,3 +49,31 @@ class Player:
 
     def sayName(self):
         print("My name is " + self.name)
+
+class Discard_Pile:
+    def __init__(self):
+        cards = []
+    def add(self, card):
+        self.cards.append(card)
+
+class Community_Cards:
+    def __init__(self):
+        self.cards = []
+
+    def turn(self, deck):
+        card = deck.draw()
+        self.cards.append(card)
+        print("Turn: {}".format(card))
+
+    def burn(deck, discard_pile):
+        card = deck.draw()
+        discard_pile.add(card)
+        print("Burn: {}".format(card.show()))
+
+    def show(self):
+        for card in self.cards:
+            card.show()
+
+
+if __name__=="__main__":
+
